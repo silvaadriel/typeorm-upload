b0b8ec90310abb942934d9ab6f19b78279380864
Adriel da Silva <silvaadriel510@gmail.com>
2020-04-26 16:26:48 -0300

    Implement features

---
+++ src/database/migrations/1587665757004-CreateCategories.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+class CreateCategories1587665757004 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          { name: 'title', type: 'varchar' },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+          { name: 'updated_at', type: 'timestamp', default: 'now()' },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
+
+export default CreateCategories1587665757004;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,92 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { getCustomRepository, In, getRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csvFilePath: string): Promise<Transaction[]> {
+    const transactionsRepositories = getCustomRepository(
+      TransactionsRepository,
+    );
+    const categoriesRepository = getRepository(Category);
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', ([title, type, value, category]) => {
+      if (!title || !type || !value) return;
+
+      const transaction = {
+        title,
+        type,
+        value,
+        category,
+      };
+      transactions.push(transaction);
+      categories.push(category);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const existentCategories = await categoriesRepository.find({
+      where: { title: In(categories) },
+    });
+
+    const existentCategoriesTitles = existentCategories
+      .map((category: Category) => category.title)
+      .filter((category, index, self) => self.indexOf(category) === index);
+
+    const addCategoriesTitles = categories
+      .filter(category => !existentCategoriesTitles.includes(category))
+      .filter((category, index, self) => self.indexOf(category) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoriesTitles.map(category => ({
+        title: category,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existentCategories];
+
+    const createdTransactions = transactionsRepositories.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepositories.save(createdTransactions);
+
+    await fs.promises.unlink(csvFilePath);
+
+    return createdTransactions;
   }
 }
 
